af7c3924339ae676632ce1d825328ca29fddbabc
Andrew Lewell <andrew.lewell@gmail.com>
2020-02-24 16:08:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function findMinAndRemoveSorted(array) {
 function merge(array1, array2) {
   let sorted = [];
   while (array1.length !== 0 && array2.length !== 0) {
-    
+    if (array1[0] > array2[0]) {
+      return 
+    }
   }
 }
 
