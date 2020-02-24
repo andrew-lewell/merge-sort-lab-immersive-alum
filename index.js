cfaffccd625bc0cbfb30f3f0598ce9243e63b1cc
Andrew Lewell <andrew.lewell@gmail.com>
2020-02-24 16:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function merge(array1, array2) {
   let sorted = [];
   while (array1.length !== 0 && array2.length !== 0) {
     if (array1[0] > array2[0]) {
-      let min = findMinAndRemoveSorted(array1);
-      sorted.push(min)
+      let min1 = findMinAndRemoveSorted(array1);
+      sorted.push(min1)
     } else {
-      let min = findMinAndRemoveSorted(array2);
-      sorted.push(min)
+      let min2 = findMinAndRemoveSorted(array2);
+      sorted.push(min2)
     }
   }
   return sorted.concat(array1).concat(array2)
